fa92c3b2f48524db5c760536c47054a3e5f5070a
Casa <carlos_henrique.taqua@hotmail.com>
2021-07-22 00:26:44 -0300

    Created enum client type

---
+++ src/com/teste/br/ClienteTipo.kt
@@ -0,0 +1,8 @@
+package com.teste.br
+
+enum class ClienteTipo(val descricao: String) {
+    PF("Pessoa Fisica"),
+    PJ("Pessoa Juridica")
+
+
+}
---
+++ src/com/teste/br/testes/TesteTipoEnum.kt
@@ -0,0 +1,20 @@
+package com.teste.br.testes
+
+import com.teste.br.ClienteTipo
+
+fun main(){
+    ClienteTipo.values().forEach {elemento ->
+        println("${elemento.name} - ${elemento.descricao}")
+    }
+
+    ClienteTipo.values().forEach {
+        println("${it.name} - ${it.descricao}")
+    }
+
+    val pf = ClienteTipo.PF
+    println("${pf.name}-${pf.descricao}")
+
+    val pj = ClienteTipo.PJ
+    println("${pj.name}-${pj.descricao}")
+
+}
